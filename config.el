5f24ff205d97dc9bb604cedce839b749e4c12654
jace <jacegem@gmail.com>
2021-11-28 19:48:53 +0900

    Add utf-8

--- config.el
+++ config.el
@@ -56,3 +56,8 @@
 
 ;; Basic
 (set-language-environment "Korean")
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
